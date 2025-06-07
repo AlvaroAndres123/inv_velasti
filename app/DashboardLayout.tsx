1fed1fb8ba2ac2c902f9d5a0654413c4457049ea
Alvaro Casco <45768376+AlvaroAndres123@users.noreply.github.com>
2025-06-06 19:57:47 -0600

    Compiendo error para desplegar en vercel

--- app/DashboardLayout.tsx
+++ app/DashboardLayout.tsx
@@ -2,15 +2,22 @@
 
 import { usePathname } from 'next/navigation';
 import Sidebar from '@/components/Sidebar';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const ocultarSidebar = pathname === '/login';
 
+  const [sidebarAbierto, setSidebarAbierto] = useState(false);
+
   return (
     <div className="flex">
-      {!ocultarSidebar && <Sidebar />}
+      {!ocultarSidebar && (
+        <Sidebar
+          abierto={sidebarAbierto}
+          setAbierto={setSidebarAbierto}
+        />
+      )}
       <main className="flex-1 min-h-screen bg-gray-50">{children}</main>
     </div>
   );
